00d8e3166b516d63f145d7207133ec90f03413e7
ogolla23 <leonardogolla23@gmail.com>
2023-10-31 01:01:55 -0700

    update 1.0

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,12 +7,13 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+binary_tree_t *node = node;
 if (!parent)
 {
 return (NULL);
 }
 
-binary_tree_t *node = binary_tree_node(parent, value);
+node = binary_tree_node(parent, value);
 if (!node)
 {
 return (NULL);
